5b48d0cb7e537bbb7625b5b1b0a3351d179a08ce
Roman Novozhilov <romannovojilov@mail.ru>
2019-07-12 23:40:11 +0300

    Setup and Initialize

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PATHS = {
+    src: path.join(__dirname, './src'),
+    dist: path.join(__dirname, './dist')
+}
+
+let config = {
+    entry: `${PATHS.src}/index.js`,
+    output: {
+        path: PATHS.dist,
+        filename: '[name].js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.?css$/,
+                use: [{
+                    loader: 'css-loader'
+                }, {
+                    loader: 'sass-loader'
+                }]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader'
+                ],
+            }
+        ]
+    },
+    devServer: {
+        contentBase: PATHS.dist,
+        compress: true,
+        port: 8081,
+        overlay: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: `index.html`,
+            template: `${PATHS.src}/index.html`
+        })
+    ]
+};
+
+module.exports = (env, options) => {
+    return config;
+}
